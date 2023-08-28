ed424cc7dbce4b8755e5784e6f77ce8872db4d2c
Vincent Auzias <codeur.auzias@gmail.com>
2023-08-28 15:45:57 +0200

    admin

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -119,4 +119,8 @@ class Article
 
         return $this;
     }
+
+    public function __toString() {
+        return $this->title;
+    }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -206,4 +206,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function __toString() {
+        return $this->email;
+    }
 }
